e2ac9c014cc1d0dcd27b966f22c0d47b70386b9a
kriszhadvicefia <fiokjaim420@proton.me>
2025-12-14 22:03:31 +0100

    added a basic lobby ui

--- scripts/playScenes/mainMenu.gd
+++ scripts/playScenes/mainMenu.gd
@@ -1,15 +1,9 @@
 extends Node2D
-<<<<<<< HEAD
 
 
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Test.tscn")
-
-
-
+	get_tree().change_scene_to_file("res://scenes/playScenes/lobby.tscn")
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
-=======
->>>>>>> 1c1080753ff885931dc1c7725e4ac1c3277ca8f5
