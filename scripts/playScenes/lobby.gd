f9606c38457a812c52d7cece8808703ea7f01301
asztalosm <23d_asztalosm@nyirszikszi.hu>
2025-12-14 23:56:56 +0100

    avatar loading

--- scripts/playScenes/lobby.gd
+++
@@ -1,4 +0,0 @@
-extends Node2D
-
-func _on_leave_lobby_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/playScenes/mainMenu.tscn")
--- scripts/play_scenes/main_menu.gd
+++ scripts/play_scenes/main_menu.gd
@@ -1,9 +1,8 @@
 extends Node2D
 
-
-
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/playScenes/lobby.tscn")
+	get_tree().change_scene_to_file("res://scenes/play_scenes/lobby_selector.tscn")
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+	
