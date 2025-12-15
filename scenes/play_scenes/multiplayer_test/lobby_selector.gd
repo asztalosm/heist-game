18dab207f19bb3e8cd975d26a73bb04c7ef90535
kriszhadvicefia <fiokjaim420@proton.me>
2025-12-15 19:00:51 +0100

    fixed lobbies

--- scenes/play_scenes/multiplayer_test/lobby_selector.gd
+++ scenes/play_scenes/multiplayer_test/lobby_selector.gd
@@ -13,11 +13,8 @@ func spawn(data):
 
 func _on_button_pressed():
 	Steam.createLobby(Steam.LOBBY_TYPE_PUBLIC)
-	$CreateLobby.hide()
-	$JoinLobby.hide()
-	$TextEdit.hide()
 
-	
+
 func _on_lobby_created(success, id):
 	if not success:
 		print("baj van")
@@ -30,26 +27,22 @@ func _on_lobby_created(success, id):
 	Steam.setLobbyJoinable(lobby_id, true)
 
 	peer.create_host()
+	multiplayer.multiplayer_peer = peer
 	
 	var label = Label.new()
 	label.text = str(lobby_id)
 	label.position = Vector2(20, 20)
 	add_child(label)
-		
+
 func _on_lobby_joined(lobby_id, steam_id, connect_result, is_friend):
-	print("VALAMI KURVA JO ", lobby_id)
-	print("BELEPETT VALAKI" , Steam.getPersonaName())
+	print("ebbe a lobbyBA LEPTEL", lobby_id)
 	multiplayer.multiplayer_peer = peer
-	
 	_display_lobby_members(lobby_id)
+	$CreateLobby.hide()
 
 func _on_join_lobby_pressed() -> void:
-	var id_to_join = $TextEdit.text.strip_edges()
-	id_to_join = int(id_to_join)
-	peer.connect_to_lobby(id_to_join)
-	
-	
-
+	var id_to_join = int($TextEdit.text.strip_edges())
+	Steam.joinLobby(id_to_join)
 
 func _display_lobby_members(lobby_id):
 	var num_of_players = Steam.getNumLobbyMembers(lobby_id)
@@ -58,4 +51,4 @@ func _display_lobby_members(lobby_id):
 	for i in range(num_of_players):
 		var steam_id = Steam.getLobbyMemberByIndex(lobby_id, i)
 		var username = Steam.getFriendPersonaName(steam_id)
-		print("Member", i, ":", username)
+		print("ember ", i, ":", username)
