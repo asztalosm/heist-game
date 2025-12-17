646bd7302354430aa93dde6878c5732b6e9cdc71
asztalosm <23d_asztalosm@nyirszikszi.hu>
2025-12-17 20:48:41 +0100

    player rotation added

--- scenes/play_scenes/player/player_scripts/player.gd
+++ scenes/play_scenes/player/player_scripts/player.gd
@@ -1,6 +1,6 @@
 class_name Player extends CharacterBody2D
 
-const SPEED = 700
+const SPEED = 300
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -9,5 +9,5 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
-
+	rotate(get_angle_to(get_global_mouse_position()) + 0.5 * PI)
 	move_and_slide()
--- scripts/play_scenes/main_menu.gd
+++ scripts/play_scenes/main_menu.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
 func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/play_scenes/multiplayer_test/lobby_selector.tscn")
+	get_tree().change_scene_to_file("res://scenes/play_scenes/Game.tscn")
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_play_button_2_pressed() -> void:
+	#temporary button, only used for me to work on the lobby stuff
+	get_tree().change_scene_to_file("res://scenes/play_scenes/multiplayer_test/lobby_selector.tscn")
