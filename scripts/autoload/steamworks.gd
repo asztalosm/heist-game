b75994bec4290bb50d652a71fa8adefd21e98873
asztalosm <23d_asztalosm@nyirszikszi.hu>
2025-12-19 23:39:56 +0100

    3/a lot

--- scripts/autoload/steamworks.gd
+++ scripts/autoload/steamworks.gd
@@ -10,31 +10,47 @@ var steam_image_texture: ImageTexture
 var lobby_data = []
 var fake_ip_data = []
 var lobby_created = false
+var peer
 
 func _process(_delta: float) -> void:
 	Steam.run_callbacks()
 
+func _on_player_joined(join_steam_id: int) -> void:
+	print(join_steam_id)
+	#Steam.getPlayerAvatar(2, join_steam_id)
+	#Steam.avatar_image_loaded.connect(func load_avatars(_avatar_id: int, size: int, data: PackedByteArray) -> void: #inline function
+	#	var steam_image: Image = Image.create_from_data(size, size, false,Image.FORMAT_RGBA8, data)
+	#	steam_image_texture = ImageTexture.create_from_image(steam_image)
+	#	print("image loaded"))
+	lobby_data.append({"role": "player", "steam_id": join_steam_id, "steam_username": Steam.getFriendPersonaName(join_steam_id)})
+
+func _on_lobby_created(result:int, new_lobby_id: int) -> void:
+	if result == Steam.Result.RESULT_OK:
+		lobby_id = new_lobby_id
+		lobby_data.append({"role": "host", "steam_id": steam_id, "steam_username": steam_username, "steam_image_texture": steam_image_texture})
+		peer = SteamMultiplayerPeer.new()
+		peer.server_relay = true
+		peer.create_host()
+		multiplayer.multiplayer_peer = peer
+		multiplayer.peer_connected.connect(_on_player_joined)
 
 
 func create_lobby() -> void:
 	lobby_data.clear()
-	lobby_id = steam_id
-	lobby_data.append({"lobby_place": 1, "steam_id": steam_id, "steam_username": steam_username, "steam_image_texture": steam_image_texture})
 	Steam.createLobby(Steam.LOBBY_TYPE_FRIENDS_ONLY, 4)
 	print(lobby_data)
-	Steam.beginAsyncRequestFakeIP(1)
-	Steam.fake_ip_result.connect(func fake_ip(result: int, remote_fake_steam_id: int, fake_ip: String, port_list: Array) -> void:
-		fake_ip_data = [result, remote_fake_steam_id, fake_ip, port_list]
-		lobby_created = true)
+	
 
 func join_lobby(this_lobby_id: int, this_steam_id: int) -> void:
 	print("lobby join", this_lobby_id, this_steam_id)
 
 func _on_lobby_chat_update(this_lobby_id: int, changed_id: int, making_change_id: int, chat_state: int) -> void:
+	print(this_lobby_id,"   ", lobby_id)
 	print(this_lobby_id, changed_id, making_change_id, chat_state)
 
 func set_user_variables() -> void:
 	steam_id = Steam.getSteamID()
+	print("client steam id: ", steam_id)
 	steam_username = Steam.getPersonaName()
 	Steam.getPlayerAvatar()
 	Steam.avatar_loaded.connect(func load_avatars(_avatar_id: int, size: int, data: PackedByteArray) -> void: #inline function
@@ -43,6 +59,7 @@ func set_user_variables() -> void:
 		print("image loaded"))
 	steam_initialized = true
 	Steam.join_requested.connect(join_lobby)
+	Steam.lobby_created.connect(_on_lobby_created)
 	Steam.lobby_chat_update.connect(_on_lobby_chat_update)
 
 
