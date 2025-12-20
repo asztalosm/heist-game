ce301e24425fcbf4e7ad7819bccb97741e9e60c7
kriszhadvicefia <fiokjaim420@proton.me>
2025-12-20 17:18:08 +0100

    shooting

--- scenes/play_scenes/player/player_scripts/event_controller.gd
+++ scenes/play_scenes/player/player_scripts/event_controller.gd
@@ -1,3 +1,5 @@
 extends Node
 
 signal cash_collected(value: int)
+signal health_changed(diff: int)
+signal health_depleted
--- scenes/play_scenes/player/player_scripts/player.gd
+++ scenes/play_scenes/player/player_scripts/player.gd
@@ -2,6 +2,7 @@ class_name Player extends CharacterBody2D
 
 @onready var player = $"."
 @onready var sprite = $Sprite2D
+var Bullet : PackedScene = preload("res://scenes/bullet.tscn")
 const SPEED = 300
 var pistol_in_hand = false
 var player_sprite = preload("res://resources/textures/sprites/idle.png")
@@ -16,7 +17,7 @@ func _physics_process(delta: float) -> void:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	rotate(get_angle_to(get_global_mouse_position()) + 0.5 * PI)
 	move_and_slide()
-	
+
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("switch_to_pistol"):
 		if pistol_in_hand == false:
@@ -25,3 +26,11 @@ func _input(event: InputEvent) -> void:
 		else:
 			sprite.texture = player_sprite
 			pistol_in_hand = false
+	if Input.is_action_just_pressed("fire"):
+		fire()
+
+func fire():
+	if pistol_in_hand == true:
+		var b = Bullet.instantiate()
+		owner.add_child(b)
+		b.transform = $Muzzle.global_transform
